71ce81e9572931f3e4cd7c09004ddf9b51c658ad
Truimo <xiaomo@20mo.cn>
2022-09-24 23:24:31 +0800

    contact view

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,7 +1,11 @@
 function Index() {
     return (
         <div className='view'>
-            contact view
+            <div className='markdown'>
+                <h2>联系方式</h2>
+                <p>邮箱：<a href='mailto:xiaomo@truimo.com'>xiaomo@truimo.com</a></p>
+                <p>QQ：3201719830</p>
+            </div>
         </div>
     )
 }
