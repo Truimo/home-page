c4253d7b823a305d419e92e98d171d18e65a8f93
Truimo <xiaomo@20mo.cn>
2022-09-24 16:54:02 +0800

    readme markdown

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,21 @@
 import './index.css';
+import ReactMarkdown from "react-markdown";
+import {useEffect, useState} from "react";
+import request from "../../request";
 
 function Index() {
+    const [markdown, setMarkdown] = useState('');
+    useEffect(() => {
+        request.get('https://raw.githubusercontent.com/Truimo/Truimo/main/README.md')
+            .then(res => {
+                setMarkdown(res.data.toString())
+            })
+    }, [])
+
     return (
         <div className='view'>
-            <p>README.md</p>
-            <div className='readme'>
-
+            <div className='markdown'>
+                <ReactMarkdown children={markdown}></ReactMarkdown>
             </div>
         </div>
     )
